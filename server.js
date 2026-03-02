56c07d14c2025a53354d5090ebf9fcca83ec98fc
jenobrock <jenovicnginamaut@gmail.com>
2026-03-02 14:49:56 +0100

    ...

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./src/app');
 const { connectDB } = require('./src/config/database');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 connectDB()
   .then(() => {
